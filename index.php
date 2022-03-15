52b93a781d64b6c3734fcb64dd8452a655bd56a2
mysteriou <elio007@hotmail.fr>
2022-03-15 06:19:10 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+
+<html lang = "fr">
+
+<head>
+
+</head>
+
+<body>
+
+<?php 
+
+/*
+Theme Name: theme_hiki
+Theme URI: https://hikikomori-france.fr/
+Author: massa anthony
+Author URI: https://www.monsite.com/monsite
+Description:  theme du site hikikomori_france
+Version: 1.0
+*/
+
+?>
+
+
+</body>
+</html>
