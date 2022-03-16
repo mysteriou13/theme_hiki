006810f0e7a83cf75c03e8bef508496910cce677
mysteriou <elio007@hotmail.fr>
2022-03-16 06:37:49 +0100

    ajout  ul et li dans le menu nav dans le header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <header>
-header
+
 <head>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -15,13 +15,35 @@ header
 <div class = "d-flex  justify-content-between">
 
 <div>
-icone
+
+<img src="https://hikikomori-france.fr/wp-content/uploads/2019/06/cropped-petitlogopng.png" class="custom-logo" alt="Hikikomori France" width="48" height="48">
+
 </div>
 
 <nav> 
 
-<ul>
-    <li> acceuil </li>
+<ul class = "d-flex">
+
+    <li> Pubs Projets de reclus/hiki</li>
+
+    <li>Catégories</li>
+    
+    <li>F.A.Q.</li>
+
+    <li> Contact</li>
+
+    <li>Mon profil</li>
+
+        <li> Connexion</li>
+
+        <li> Inscription </li>
+
+        <li> Membres </li>
+
+   <li> Page officielle Facebook</li>
+       
+    <lli> Accueil </li>
+
 </ul>
 </nav>
 
@@ -29,7 +51,7 @@ icone
 </div>
 
 <div>
-    image
+    
 </div>
 
 </div>
