1b2aa927b9aa3e9b5957fed84cea2c778c4eb234
mysteriou <elio07@hotmail.fr>
2023-01-11 23:30:20 +0100

    ajout css mobile pour le formulaire login

--- formulaire/form_login.php
+++ formulaire/form_login.php
@@ -2,7 +2,7 @@
 <form name="loginform" id="loginform" action="<?php echo $login_url; ?>"  method="post">
 
 
-  <div class = "d-flex  justify-content-evenly">
+  <div class = "form_login">
 			<div class="login-username">
 				<label for="user_login" class = "link-light">
     
@@ -11,14 +11,24 @@
 </div>
 
 			<div class="login-password">
-				<label for="user_pass " class = "link-light">Mot de passe</label>
+			<div>	
+			<label for="user_pass " class = "link-light">Mot de passe</label>
+           </div>
+
+		      <div>
 				<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" />
+              </div>
 </div>
 			
 			<div class="login-remember"> <label class = "link-light"/> <input name="rememberme" type="checkbox" id="rememberme" value="forever"  > Se souvenir de moi</label>
 		
+			<div>
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary" value="Se connecter" />
+               </div>
+
+			   <div>
 				<input type="hidden" name="redirect_to" value="<?php echo $index?>" />
+</div>
 </div>
 
 <div>
