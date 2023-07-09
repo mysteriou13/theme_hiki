3437189f6c52258d0d3f4c0f19776683c93bc14a
mysteriou <elio07@hotmail.fr>
2023-07-09 17:36:16 +0200

    ajout width:100vw dans le footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer id = "footer">
+<footer class = "footer">
 
 
 <div class = "box_footer">
@@ -11,65 +11,5 @@
 
 </div>
 
-<?php 
 
-require_once($full_theme_hiki."/js/main_js.php");
-
-require_once($full_theme_hiki."/js/main_js.php");
-
-?>
-
-</footer>
-<script>
-
-var allwidthscreen = screen.width; 
-
-var newwidth = (allwidthscreen * 95) / 100;
-
-var cardwidth = (allwidthscreen * 70) / 100;
-
-var catid = "#liste_category";
-
-if(screen.width <= 800){
-
- catid = "#mobilecat";
-
-}
-
-
-
-
-
-
-
-
-    let params = new URLSearchParams(document.location.search);
-   let name = params.get("p");
-
-   let cat = params.get("cat");
-
-   let login = params.get("login");
-
-    console.log(name);
-
-    if(login !== null){
- 
-        
-
-    }
-    
-
-    if(screen.width <= 400){
-
-        $(".aside").width('94%');
-
-    }
-
-
-    $("#mainbox").width('94%');
-
-    $("#footer").width( screen.width);
-    
-
-    </script>
 
